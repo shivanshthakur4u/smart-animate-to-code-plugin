306e72f608ef826259f0c3898bc97fd353a53f5e
Saurabh Singh <boyslike2play@gmail.com>
2023-04-21 16:29:18 +0530

    completed

---
+++ src/utils/figma-utils.ts
@@ -0,0 +1,34 @@
+// Utility functions for Figma plugin
+
+// Function to calculate the translation between two components
+function calculateTranslation(mainComponent: ComponentNode, variantComponent: ComponentNode) {
+    const dx = variantComponent.x - mainComponent.x;
+    const dy = variantComponent.y - mainComponent.y;
+    return { dx, dy };
+  }
+  
+  // Function to generate the Smart Animation code
+  function generateCode(mainComponent: ComponentNode, variantComponent: ComponentNode, translation: { dx: number, dy: number }) {
+    const mainComponentKey = mainComponent.key;
+    const variantComponentKey = variantComponent.key;
+    const code = `// Smart Animation code for Figma Components:
+  // Main Component: "${mainComponent.name}" (key: ${mainComponentKey})
+  // Variant Component: "${variantComponent.name}" (key: ${variantComponentKey})
+  
+  const mainComponent = figma.root.findOne(node => node.type === 'COMPONENT' && node.key === '${mainComponentKey}');
+  const variantComponent = figma.root.findOne(node => node.type === 'COMPONENT' && node.key === '${variantComponentKey}');
+  const translation = { dx: ${translation.dx}, dy: ${translation.dy} };
+  
+  // Apply translation to variant component
+  variantComponent.x += translation.dx;
+  variantComponent.y += translation.dy;
+  `;
+  
+    return code;
+  }
+  
+  export {
+    calculateTranslation,
+    generateCode
+  };
+  
